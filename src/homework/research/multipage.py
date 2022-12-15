6e0f4c4060a7baddcd5d575057b36c7832f64fd3
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-12-15 19:23:18 +0100

    Reorganise Repo

---
+++ src/homework/research/multipage.py
@@ -0,0 +1,60 @@
+import time
+from math import pi
+
+import flet
+from flet import Column, Icon, Page, Row, Text, icons
+
+from controls.menus.collapsible import Collapsible
+from controls.menus.menu_button import MenuButton
+
+
+def main(page: Page):
+
+    page.scroll = "auto"
+    page.add(
+        Column(
+            [
+                Collapsible(
+                    "Buttons",
+                    icon=Icon(icons.SMART_BUTTON),
+                    content=Column(
+                        [
+                            MenuButton("Basic buttons"),
+                            MenuButton("Floating action button"),
+                            MenuButton("Popup menu button", selected=True),
+                        ],
+                        spacing=3,
+                    ),
+                ),
+                Collapsible(
+                    "Simple apps",
+                    icon=Icon(icons.NEW_RELEASES),
+                    content=Column(
+                        [
+                            MenuButton("Basic buttons"),
+                            MenuButton("Floating action button"),
+                            MenuButton("Popup menu button", selected=True),
+                        ],
+                        spacing=3,
+                    ),
+                ),
+                Collapsible(
+                    "Forms",
+                    icon=Icon(icons.DYNAMIC_FORM),
+                    content=Column(
+                        [
+                            MenuButton("Basic buttons"),
+                            MenuButton("Floating action button"),
+                            MenuButton("Popup menu button", selected=True),
+                        ],
+                        spacing=3,
+                    ),
+                ),
+            ],
+            spacing=3,
+            width=930,
+        )
+    )
+
+
+flet.app(port=8550, target=main, view=flet.WEB_BROWSER)
