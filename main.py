484ab5412e709bf9a63fe2416e12343cd3dae726
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-12-15 12:28:21 +0100

    Inital commit

---
+++ main.py
@@ -0,0 +1,398 @@
+""" these are the modules needed"""
+import flet
+from flet import (
+    Page,
+    Text,
+    View,
+    Column,
+    Container,
+    LinearGradient,
+    alignment,
+    border_radius,
+    json,
+    padding,
+    Row,
+    Card,
+    TextField,
+    FilledButton,
+    SnackBar,
+)
+
+from datetime import datetime
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def main(page: Page):
+    page.title = "Routes Example"
+    snack = SnackBar(
+        Text("Registration successfull!"),
+    )
+
+    def GradientGenerator(start, end):
+        ColorGradient = LinearGradient(
+            begin=alignment.bottom_left,
+            end=alignment.top_right,
+            colors=[
+                start,
+                end,
+            ],
+        )
+
+        return ColorGradient
+
+    def req_healthcheck(e):
+        client = requests.session()
+
+        # Define the endpoint URL
+        url = "http://127.0.0.1:8000/api/healthcheck"
+
+        headers = {"accept": "*/*"}
+
+        response = client.get(url, headers=headers)
+
+        if response.status_code == 200:
+
+            page.views.append(
+                View(
+                    f"/{response.status_code}",
+                    horizontal_alignment="center",
+                    vertical_alignment="center",
+                    controls=[
+                        Column(
+                            alignment="center",
+                            horizontal_alignment="center",
+                            controls=[
+                                Text(
+                                    "response was 200!",
+                                    size=44,
+                                    weight="w700",
+                                    text_align="center",
+                                ),
+                                Text(
+                                    # We can show the user info by using the string
+                                    f"Health Check  {response.json()}",
+                                    size=32,
+                                    weight="w500",
+                                    text_align="center",
+                                ),
+                            ],
+                        ),
+                    ],
+                )
+            )
+
+        else:
+            snack.content.value = f"Health check response was {response.status_code}."
+            snack.open = True
+            page.update()
+
+        page.update()
+
+    def req_register(e, email, password):
+        # we set the email and passord as a dict/json structure in order to pass it to the request
+
+        client = requests.session()
+
+        first_name = "First"
+        last_name_1 = "Ryan"
+        last_name_2 = "Rodriguez"
+        description = ""
+        is_learner = True
+        is_guardian = False
+        is_staff = False
+        date_joined = datetime.now()
+
+        data = {
+            "email": "peter@davidjnevin.com",
+            "password": "Password10!",
+            "first_name": first_name,
+            "last_name_1": last_name_1,
+            "last_name_2": last_name_2,
+            "description": description,
+            "is_learner": is_learner,
+            "is_guardian": is_guardian,
+            "is_staff": is_staff,
+            "date_joined": "2022-10-28T23:23:23Z",
+        }
+
+        response = client.post(
+            "http://127.0.0.1:8000/api/users/users",
+            json=data,
+            headers={"accept": "*/*"},
+        )
+
+        if response.status_code == 201:
+            snack.content.value = "You are registered! Check your email."
+            snack.open = True
+            page.update()
+
+            """ we can check if the user info was stored in the db correctly
+                So the user correclty was inserted in the db using the Python API
+            """
+        else:
+            snack.content.value = f"The response was {response.status_code}, {response.content}  Try again."
+            snack.open = True
+            page.update()
+
+    def req_login(e, email, password):
+        data = {
+            "email": email,
+            "password": password,
+        }
+        client = requests.session()
+        # Define the endpoint URL
+        url = "http://127.0.0.1:8000/api/users/login"
+
+        response = client.post(
+            url, json={"email": email, "password": password}, headers={"accept": "*/*"}
+        )
+
+        if response.status_code == 200:
+
+            page.views.append(
+                View(
+                    f"/{email}",
+                    horizontal_alignment="center",
+                    vertical_alignment="center",
+                    controls=[
+                        Column(
+                            alignment="center",
+                            horizontal_alignment="center",
+                            controls=[
+                                Text(
+                                    "Succssesfuly Logged in!",
+                                    size=44,
+                                    weight="w700",
+                                    text_align="center",
+                                ),
+                                Text(
+                                    # We can show the user info by using the string
+                                    f"Login Information:\nEmail: {email}\nPassword: {password}",
+                                    size=32,
+                                    weight="w500",
+                                    text_align="center",
+                                ),
+                            ],
+                        ),
+                    ],
+                )
+            )
+
+        else:
+            snack.content.value = (
+                f"Could not log in! response was {response.status_code}."
+            )
+            snack.open = True
+            page.update()
+
+        page.update()
+
+    def route_change(route):
+        email = TextField(
+            label="Email",
+            border="underline",
+            width=320,
+            text_size=14,
+        )
+
+        password = TextField(
+            label="Password",
+            border="underline",
+            width=320,
+            text_size=14,
+            password=True,
+            can_reveal_password=True,
+        )
+
+        page.views.clear()
+        """ we will be working with views, each 'page' will be a seperate view, i..e login page, reg page, etc..
+
+        """
+
+        # start with the registration page
+        page.views.append(
+            View(
+                "/register",
+                horizontal_alignment="end",
+                vertical_alignment="center",
+                padding=padding.only(right=160),
+                controls=[
+                    Column(
+                        alignment="center",
+                        controls=[
+                            Card(
+                                elevation=15,
+                                content=Container(
+                                    width=550,
+                                    height=550,
+                                    padding=padding.all(30),
+                                    gradient=GradientGenerator("#000000", "#111111"),
+                                    border_radius=border_radius.all(12),
+                                    content=Column(
+                                        horizontal_alignment="center",
+                                        alignment="start",
+                                        controls=[
+                                            Text(
+                                                "Homework Helper",
+                                                size=32,
+                                                weight="w700",
+                                                text_align="center",
+                                            ),
+                                            Container(padding=padding.only(bottom=20)),
+                                            email,
+                                            Container(padding=padding.only(bottom=10)),
+                                            password,
+                                            Container(padding=padding.only(bottom=20)),
+                                            Row(
+                                                alignment="center",
+                                                spacing=20,
+                                                controls=[
+                                                    # the UI above can be replicated, but most importnatly is here:
+                                                    # we pass the email and password values to the req_register function
+                                                    FilledButton(
+                                                        content=Text(
+                                                            "Register",
+                                                            weight="w700",
+                                                        ),
+                                                        width=160,
+                                                        height=40,
+                                                        # pass the input values to the function that sends HTTP requests
+                                                        on_click=lambda e: req_registter(
+                                                            e,
+                                                            email.value,
+                                                            password.value,
+                                                        ),
+                                                    ),
+                                                    FilledButton(
+                                                        content=Text(
+                                                            "Login",
+                                                            weight="w700",
+                                                        ),
+                                                        width=160,
+                                                        height=40,
+                                                        on_click=lambda __: page.go(
+                                                            "/login"
+                                                        ),
+                                                    ),
+                                                    snack,
+                                                ],
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                            )
+                        ],
+                    )
+                ],
+            )
+        )
+
+        if page.route == "/login":
+            page.views.append(
+                View(
+                    "/login",
+                    horizontal_alignment="center",
+                    vertical_alignment="center",
+                    controls=[
+                        Column(
+                            alignment="center",
+                            controls=[
+                                Card(
+                                    elevation=15,
+                                    content=Container(
+                                        width=550,
+                                        height=550,
+                                        padding=padding.all(30),
+                                        gradient=GradientGenerator("#000", "#111"),
+                                        border_radius=border_radius.all(12),
+                                        content=Column(
+                                            horizontal_alignment="center",
+                                            alignment="start",
+                                            controls=[
+                                                Text(
+                                                    "Homework Helper",
+                                                    size=32,
+                                                    weight="w700",
+                                                    text_align="center",
+                                                ),
+                                                Container(
+                                                    padding=padding.only(bottom=20)
+                                                ),
+                                                email,
+                                                Container(
+                                                    padding=padding.only(bottom=10)
+                                                ),
+                                                password,
+                                                Container(
+                                                    padding=padding.only(bottom=20)
+                                                ),
+                                                Row(
+                                                    alignment="center",
+                                                    spacing=20,
+                                                    controls=[
+                                                        FilledButton(
+                                                            content=Text(
+                                                                "Login",
+                                                                weight="w700",
+                                                            ),
+                                                            width=160,
+                                                            height=40,
+                                                            # Now, if we want to login, we also need to send some info back to the server and check if the credentials are correct or if they even exists.
+                                                            on_click=lambda e: req_login(
+                                                                e,
+                                                                email.value,
+                                                                password.value,
+                                                            ),
+                                                        ),
+                                                        FilledButton(
+                                                            content=Text(
+                                                                "healthcheck",
+                                                                weight="w700",
+                                                            ),
+                                                            width=160,
+                                                            height=40,
+                                                            on_click=lambda e: req_healthcheck(
+                                                                e,
+                                                            ),
+                                                        ),
+                                                        FilledButton(
+                                                            content=Text(
+                                                                "Register",
+                                                                weight="w700",
+                                                            ),
+                                                            width=160,
+                                                            height=40,
+                                                            on_click=lambda e: req_register(
+                                                                e,
+                                                                email.value,
+                                                                password.value,
+                                                            ),
+                                                        ),
+                                                        snack,
+                                                    ],
+                                                ),
+                                            ],
+                                        ),
+                                    ),
+                                )
+                            ],
+                        )
+                    ],
+                )
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+
+# we can now test this using the web browser
+flet.app(target=main, host="localhost", port=9999, view=flet.WEB_BROWSER)
