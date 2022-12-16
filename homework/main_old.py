5848fea6ea657eb783c9cbe89e648ae6338044ec
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-12-16 01:15:47 +0100

    Add appbar

--- homework/main_old.py
+++ homework/main_old.py
@@ -47,27 +47,6 @@ def main(page: Page):
         e.control.checked = not e.control.checked
         page.update()
 
-    menu = AppBar(
-        leading=Icon(icons.PALETTE),
-        leading_width=40,
-        title=Text("AppBar Example"),
-        center_title=False,
-        bgcolor=colors.SURFACE_VARIANT,
-        actions=[
-            IconButton(icons.WB_SUNNY_OUTLINED),
-            IconButton(icons.FILTER_3),
-            PopupMenuButton(
-                items=[
-                    PopupMenuItem(text="Item 1"),
-                    PopupMenuItem(),  # divider
-                    PopupMenuItem(
-                        text="Checked item", checked=False, on_click=check_item_clicked
-                    ),
-                ]
-            ),
-        ],
-    )
-
     def req_healthcheck(e):
         client = requests.session()
 
