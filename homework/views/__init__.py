37e389cae8e110a535f92d7a2c547d9fa6e26484
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-12-17 14:28:32 +0100

    Rename login_view to login

--- homework/views/__init__.py
+++ homework/views/__init__.py
@@ -1,4 +1,4 @@
 from .register import RegisterView  # noqa: F401
 from .landing_page import LandingPageView  # noqa: F401
 from .health_check_results import HealthCheckResultsView  # noqa: F401
-from .login_view import LoginView  # noqa: F401
+from .login import LoginView  # noqa: F401
