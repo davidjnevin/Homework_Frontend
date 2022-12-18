46b91f32928b612ae6b3df29adf09443795b177c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-12-18 11:57:57 +0100

    Fix API_BASE_URL

--- homework/homework/settings/default.py
+++ homework/homework/settings/default.py
@@ -3,5 +3,5 @@ import os
 from decouple import config
 
 
-API_BASE_URL = config("API_BASE_URL", default="127.0.0.1/")
+API_BASE_URL = config("API_BASE_URL", default="http://127.0.0.1:8000")  # No trailing /
 PROJECT_NAME = config("PROJECT_NAME", default="Homework")
