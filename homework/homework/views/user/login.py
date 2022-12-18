f138bea1bc42a3b51d6e18738888c7a3a96a194c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-12-18 20:46:33 +0100

    Add error catching

--- homework/homework/views/user/login.py
+++ homework/homework/views/user/login.py
@@ -1,11 +1,8 @@
-from datetime import datetime
 from flet import AppBar, ElevatedButton, Text, View, colors
-from pydantic import ValidationError
 from homework.models.users import UserLoginModel
 
-from email_validator import EmailNotValidError
-import ast
-from homework.components import FormFields
+from email_validator import EmailNotValidError, validate_email
+from homework.components import FormFields as ffs
 from homework.data import UserApiCalls as apicall
 
 
@@ -13,11 +10,16 @@ class LoginView:
     def login_view(e, page):
         """Login view."""
 
-        txt_email = FormFields.text_field("Your Email")
-        txt_password = FormFields.password_field("Your Password")
+        txt_email = ffs.text_field("Your Email")
+        txt_password = ffs.password_field("Your Password")
+        api_error_notifcation = ffs.text_alert(
+            "We cannot find a user for that email address and that password. Please try again."
+        )
 
         def login_btn_click(e):
+            api_error_notifcation.visible = False
             view.clean()
+            view.update()
             if not txt_email.value:
                 txt_email.error_text = "Please enter an email address."
             else:
@@ -30,12 +32,13 @@ class LoginView:
 
             if txt_email.value and txt_password.value:
                 try:
+                    validation = validate_email(txt_email.value)
+                    email = validation.email
                     login_data = UserLoginModel(
-                        email=txt_email.value, password=txt_password.value
+                        email=email, password=txt_password.value
                     )
-                except ValidationError as error:
-                    error_text = ast.literal_eval(error.json())[0]["msg"]
-                    txt_email.error_text = error_text
+                except EmailNotValidError as error:
+                    txt_email.error_text = str(error)
                     login_data = False
                     page.update()
 
@@ -44,27 +47,33 @@ class LoginView:
                         response = apicall.login_user(login_data)
                         if response.status_code == 200:
                             page.go("/welcome")
-                        else:
-                            api_connection_error = FormFields.text_field(
-                                response.content
-                            )
-                            page.controls.append(api_connection_error)
-                    except Exception as e:
-                        api_connection_error = FormFields.text_field(
-                            f'{str(Exception("There is a problem connecting to the server. Please try again in a few mintes."))}'
-                        )
-                        view.controls.append(api_connection_error)
+                        if response.status_code == 401:
+                            print("Response in btn_click: ", response.status_code)
+                            api_error_notifcation.visible = True
+                            txt_email.error_text = ""
+                            txt_password.error_text = ""
+                            txt_email.value = ""
+                            txt_password.value = ""
+                            view.update()
 
+                    except Exception as e:
+                        api_connection_error_message = f'{str(e("There is a problem connecting to the server. Please try again in a few mintes."))}'
+                        api_connection_error_notification = ffs.text_alert(
+                            api_connection_error_message
+                        )
+                        api_connection_error_notification.visible = True
+                        view.update()
             page.update()
 
         view = View(
             "/",
             controls=[
                 AppBar(title=Text("Login"), bgcolor=colors.SURFACE_VARIANT),
+                api_error_notifcation,
                 txt_email,
                 txt_password,
                 ElevatedButton("Login", on_click=login_btn_click),
-                ElevatedButton("Visit Welcome", on_click=lambda _: page.go("/welcome")),
+                ElevatedButton("Start", on_click=lambda _: page.go("/start")),
             ],
         )
 
